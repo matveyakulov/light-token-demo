7da19a3f61822e5675c67191d88b3422ce106f9c
Матвей Акулов <akulov-gosha@mail.ru>
2025-05-11 16:21:41 +0300

    fix

--- src/main/java/ru/matveyakulov/controller/DemoController.java
+++ src/main/java/ru/matveyakulov/controller/DemoController.java
@@ -5,15 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.matveyakulov.client.DemoClient;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
 
+    private final DemoClient demoClient;
+
     @GetMapping
     public void get() {
-        // бизнес логика
+        demoClient.post();
     }
 
     @PostMapping
